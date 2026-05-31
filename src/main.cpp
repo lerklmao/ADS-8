57b35022e17e332baed58a5da93489bb39d5358f
lerklmao <owins.12KVE@gmail.com>
2026-05-31 22:55:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,20 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include "bst.h"
+#include <string>
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> tree;
-    
     makeTree(tree, "src/war_peace.txt");
-    
     std::cout << "=== Статистика дерева ===" << std::endl;
     std::cout << "Уникальных слов: " << tree.size() << std::endl;
     std::cout << "Глубина дерева: " << tree.depth() << std::endl;
     std::cout << std::endl;
-    
     std::cout << "Сохраняю результат в result/freq.txt..." << std::endl;
     printFreq(tree);
-    
     std::cout << "Готово!" << std::endl;
-    
     return 0;
 }
